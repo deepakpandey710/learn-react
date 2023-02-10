6d51de842bab433f6740ef9bf106fc2123adf273
Deepak pandey <pandeydeepak710@gmail.com>
2023-02-10 15:37:36 +0530

    commiting did update component

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ class App extends Component{
   constructor(){
     super();
     this.state={
-      name:"deepak"
+      count:0
+    }
+    console.warn("consturctor called");
+  }
+  componentDidUpdate(preProps,preState,snapshot){
+    console.warn("componentDidMount called",preProps,preState,snapshot);
+    if(preState.count==this.state.count){
+      alert("hey brooo");
     }
   }
-  componentDidMount(){
-    console.warn('componentDidMount');
-  }
-  render()
-  {
-    console.warn('render');
+  render(){
     return(
       <div>
-        <h1>Component Did mount {this.state.name}</h1>
-        <button onClick={()=>this.setState({name:"pandey"})}>update</button>
+        <h1>component Did Update {this.state.count}</h1>
+        {/* <button onClick={()=>this.setState({count:this.state.count+1})}>update</button> */}
+        <button onClick={()=>this.setState({count:1})}>update</button>
       </div>
     )
   }
+
+
 }
 
 
