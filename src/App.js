f8ac38891c7d7b877e47ecda9f2cad57617b90b0
Deepak pandey <pandeydeepak710@gmail.com>
2023-02-10 18:39:33 +0530

    commiting will unmount component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import {Component, useState} from 'react';
 import './App.css';
-import User from './User';
+import Student from './Student';
 class App extends Component{
   constructor(){
     super();
     this.state={
-      count:0
+      show:false
     }
-    console.warn("consturctor called");
-  }
-  shouldComponentUpdate(){
-    console.log("shouldComponentUpdate");
-    // return true;
-    if(this.state.count>3 && this.state.count<10){
-      return true;
-    }
-    return false;
   }
   render(){
     return(
       <div>
-        <h1>component Did Update {this.state.count}</h1>
-        <button onClick={()=>this.setState({count:this.state.count+1})}>update</button>
-        {/* <button onClick={()=>this.setState({count:1})}>update</button> */}
+        {
+          this.state.show?<Student/>:<h1>child</h1>
+        }
+        <button onClick={()=>this.setState({show:!this.state.show})}>toggle</button>
       </div>
     )
   }
--- src/Student.js
+++ src/Student.js
@@ -1,11 +1,14 @@
 import React from "react";
 export default class Student extends React.Component{
+    componentWillUnmount(){
+        console.warn("unmount component called");
+    }
     render()
     {
         console.log(this.props)
         return (
             <div>
-                <h1>hello {this.props.name}</h1>
+                <h1>class component</h1>
             </div>
         )
     }
