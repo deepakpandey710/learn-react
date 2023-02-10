345da79286ea18b39e2b9e42cd5defa08c7f5f56
Deepak pandey <pandeydeepak710@gmail.com>
2023-02-10 17:23:51 +0530

    should update component

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ class App extends Component{
     }
     console.warn("consturctor called");
   }
-  componentDidUpdate(preProps,preState,snapshot){
-    console.warn("componentDidMount called",preProps,preState,snapshot);
-    if(preState.count==this.state.count){
-      alert("hey brooo");
+  shouldComponentUpdate(){
+    console.log("shouldComponentUpdate");
+    // return true;
+    if(this.state.count>3 && this.state.count<10){
+      return true;
     }
+    return false;
   }
   render(){
     return(
       <div>
         <h1>component Did Update {this.state.count}</h1>
-        {/* <button onClick={()=>this.setState({count:this.state.count+1})}>update</button> */}
-        <button onClick={()=>this.setState({count:1})}>update</button>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>update</button>
+        {/* <button onClick={()=>this.setState({count:1})}>update</button> */}
       </div>
     )
   }
