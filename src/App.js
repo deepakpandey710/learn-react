513593c1ec0d1ee226fbbf5319ee9371029d05a9
Deepak pandey <pandeydeepak710@gmail.com>
2023-02-13 18:17:53 +0530

    nested table with bootstrap and map function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,70 @@
-import {Component, useState} from 'react';
+import { Component, useState } from 'react';
 import './App.css';
-import {Tab, Table} from 'react-bootstrap'
-function App(){
-  const students=[
-    {name:"deepak",email:"deepak@test.com",contact:"888"},
-    {name:"siddhu",email:"sddgek@test.com",contact:"248"},
-    {name:"ritik",email:"dzdfs@test.com",contact:"348"}
-  ]
-  return(
-    <div className='App'>
-      <h1>Map function working</h1>
-      <Table border='1' striped variant="dark">
-      
-        <tbody>
-        <tr>
-          <td>name</td>
-          <td>email</td>
-          <td>contact</td>
-        </tr>
+import { Tab, Table } from 'react-bootstrap'
+function App() {
+    const user = [
         {
-          students.map((item,i)=>
-            <tr key={i}>
-              <td >{item.name}</td>
-              <td>{item.email}</td>
-              <td>{item.contact}</td>
-            </tr>
-          )
+            name: "riya", email: 'abc@test.com', address: [
+                { hn: '232', city: 'basti', country: 'India' },
+                { hn: '212', city: 'noida', country: 'India' },
+                { hn: '562', city: 'newyork', country: 'India' }
+            ]
+        },
+        {
+            name: "ritik", email: 'abc@test.com', address: [
+                { hn: '232', city: 'gkpi', country: 'India' },
+                { hn: '212', city: 'dublini', country: 'India' },
+                { hn: '562', city: 'newyork', country: 'India' }
+            ]
+        },
+        {
+            name: "deepak", email: 'abc@test.com', address: [
+                { hn: '232', city: 'bangalore', country: 'India' },
+                { hn: '212', city: 'siliconvalley', country: 'India' },
+                { hn: '562', city: 'amsterdam', country: 'India' }
+            ]
         }
-        </tbody>
-      </Table>
-    </div>
-  )
+    ]
+    return (
+        <div className='App'>
+            <h1>Listing Nested Component</h1>
+            <Table border='1' variant="dark"  >
+                <tbody>
+                    <tr>
+                        <td>S.No.</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Address</td>
+                    </tr>
+                    {
+                        user.map((item,i) =>
+                            <tr key={i}>
+                                <td>{i+1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>
+                                    <Table variant="dark" striped>
+                                        <tbody>
+                                            {
+                                                item.address.map((data,j) =>
+                                                    <tr key={j}>
+                                                        <td>{data.hn}</td>
+                                                        <td>{data.city}</td>
+                                                        <td>{data.country}</td>
+                                                    </tr>
+                                                )
+                                            }
+                                        </tbody>
+                                    </Table>
+                                </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </Table>
+
+        </div>
+    )
 }
 
 export default App;
