45abe88b5032ff090040b0cfd9f9e4eb88acba1b
Deepak pandey <pandeydeepak710@gmail.com>
2023-02-13 13:42:15 +0530

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import {Component, useState} from 'react';
 import './App.css';
-import { Button,Alert } from 'react-bootstrap';
-
 function App(){
+  const students=[
+    {name:"deepak",email:"deepak@test.com",contact:"888"},
+    {name:"siddhu",email:"sddgek@test.com",contact:"248"},
+    {name:"ritik",email:"dzdfs@test.com",contact:"348"}
+  ]
   return(
     <div className='App'>
-      <h1>install bootstrap</h1>
-      <Button onClick={()=>alert("hello")}>click me</Button>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
-      <button>hello</button>
-
-      {[
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark',
-      ].map((variant) => (
-        <Alert key={variant} variant={variant}>
-          This is a {variant} alert—check it out!
-        </Alert>
-      ))}
+      <h1>Map function working</h1>
+      <table border="1">
+        <tr>
+          <td>name</td>
+          <td>email</td>
+          <td>contact</td>
+        </tr>
+        {
+          students.map((item)=>
+            <tr>
+              <td >{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.contact}</td>
+            </tr>
+          )
+        }
+      </table>
     </div>
   )
 }
 
-
 export default App;
+
