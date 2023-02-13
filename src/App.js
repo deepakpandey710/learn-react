4df9b35e089414baf081a44888c0ff4d22eebb94
Deepak pandey <pandeydeepak710@gmail.com>
2023-02-13 14:36:29 +0530

    commmiting table using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Component, useState} from 'react';
 import './App.css';
+import {Tab, Table} from 'react-bootstrap'
 function App(){
   const students=[
     {name:"deepak",email:"deepak@test.com",contact:"888"},
@@ -9,22 +10,25 @@ function App(){
   return(
     <div className='App'>
       <h1>Map function working</h1>
-      <table border="1">
+      <Table border='1' striped variant="dark">
+      
+        <tbody>
         <tr>
           <td>name</td>
           <td>email</td>
           <td>contact</td>
         </tr>
         {
-          students.map((item)=>
-            <tr>
+          students.map((item,i)=>
+            <tr key={i}>
               <td >{item.name}</td>
               <td>{item.email}</td>
               <td>{item.contact}</td>
             </tr>
           )
         }
-      </table>
+        </tbody>
+      </Table>
     </div>
   )
 }
